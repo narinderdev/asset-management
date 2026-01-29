cdfab32aeb81e007e39b879690b9d1b36b0ee520
narinderdev <narinder.dev@gmail.com>
2026-01-29 19:07:51 +0530

    Added deployed link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://1333afe42f4d.ngrok-free.app'
+  // apiUrl: 'https://1333afe42f4d.ngrok-free.app'
+apiUrl: 'http://54.225.63.207:8081'
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://1333afe42f4d.ngrok-free.app'
+  // apiUrl: 'https://1333afe42f4d.ngrok-free.app'
+  apiUrl: 'http://54.225.63.207:8081'
 };
