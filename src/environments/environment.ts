54cb5160b5c5bea7a1ea720e1b3ae60829cdc0f0
Narinder Dev <narinder.dev@gmail.com>
2026-03-05 18:06:20 +0530

    Update API URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://7a54-203-190-154-162.ngrok-free.app'
-  // apiUrl: 'http://3.84.76.210:8081'
+  // apiUrl: 'https://7a54-203-190-154-162.ngrok-free.app'
+  apiUrl: 'http://3.82.49.182:8081'
 };
