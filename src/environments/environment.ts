b3fd8744a7351f7ffa9cff982aee284e5e6abf64
Narinder Dev <narinder.dev@gmail.com>
2026-03-05 18:05:16 +0530

    Update apiUrl to new server address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://8cea6bac72b0.ngrok-free.app'
+  // apiUrl: 'https://8cea6bac72b0.ngrok-free.app'
+  apiUrl: 'http://3.82.49.182:8081'
 };
