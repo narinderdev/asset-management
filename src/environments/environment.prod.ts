15183d158a689fc9ace11d7033b6739dd6fa2ecb
Narinder Dev <narinder.dev@gmail.com>
2026-03-05 17:58:59 +0530

    Update production API URL in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://7a54-203-190-154-162.ngrok-free.app'
-// apiUrl: 'http://3.84.76.210:8081'
+  // apiUrl: 'https://7a54-203-190-154-162.ngrok-free.app'
+apiUrl: 'http://3.82.49.182:8081'
 
 };
